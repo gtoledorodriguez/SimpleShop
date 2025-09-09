81f7452b6d543faceafe173763db3d7863a668e4
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-09-09 06:21:44 +0000

    generated views and model for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root businesses#index
+  devise_for :users
   resources :businesses
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })

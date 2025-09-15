4ccd293a5d832fbfb1945dc6f0093b161f5f77ea
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-09-15 06:40:46 +0000

    had item path be called inventory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "home/index"
   resources :sales
-  resources :items
+  resources :items, path: "inventory"
   devise_for :users
   resources :businesses
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
@@ -9,6 +8,6 @@ Rails.application.routes.draw do
 
   root "home#index"
 
-  get("/rake_tasks", { :controller => "rake_tasks", :action => "show" })
-  get("/run_task", { :controller => "rake_tasks", :action => "run_task" })
+  get "/rake_tasks", { :controller => "rake_tasks", :action => "show" } 
+  get "/run_task", { :controller => "rake_tasks", :action => "run_task" } 
 end

0d04a628b9b04650716fc8297bbd3aab7c5d8c14
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-09-17 20:05:51 +0000

    renaming quanitity_sold to quantity_sold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_062447) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_17_200254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_062447) do
   create_table "sales", force: :cascade do |t|
     t.integer "item_id"
     t.integer "user_id"
-    t.integer "quanitity_sold"
+    t.integer "quantity_sold"
     t.float "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
